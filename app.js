be9c921651c23fbf95d2c1dafd7d6ff6ba3288ac
fsialer <alexis_s309@hotmail.com>
2015-06-26 15:34:14 -0500

    express-partials
    Quiz versión 4 - instalación del express-partials en el proyecto Quiz

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var partials=require('express-partials');
 var routes = require('./routes/index');
 
 
@@ -19,6 +19,7 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(partials());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
