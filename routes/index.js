7ab5c0761ceb5f81427cd8d8f27d5613818b3910
fsialer <alexis_s309@hotmail.com>
2015-07-22 15:13:33 -0500

    Editar preguntas
    Quiz versión 12-declaración del editar preguntas en el proyecto

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var quizController=require('../controllers/quiz_controller');
 var authorController=require('../controllers/author_controller');
 /* pagina de entrada (home page). */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz',errors:[]});
 });
 //Autoload de comandos con :quizId
 router.param('quizId',quizController.load);//autoload:quizId
@@ -14,5 +14,9 @@ router.get('/quizes/:quizId(\\d+)',quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 router.get('/quizes/new', quizController.new);
 router.post('/quizes/create',quizController.create);
-router.get('/author',authorController.author);
+router.get('/quizes/:quizId(\\d+)/edit',quizController.edit);
+router.put('/quizes/:quizId(\\d+)',quizController.update);
+
+
+router.get('/author',quizController.author);
 module.exports = router;
