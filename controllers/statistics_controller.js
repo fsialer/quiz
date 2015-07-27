d1251229d52acfaf136a5b2ab96a645a7ddcccc2
fsialer <alexis_s309@hotmail.com>
2015-07-26 21:16:54 -0500

    Estadística Cambio
    Quiz version 20.1 - Codificación del controller

---
+++ controllers/statistics_controller.js
@@ -0,0 +1,41 @@
+var models = require('../models/models.js');
+
+var statistics = {
+      quizes: 0,
+      comments: 0,
+      commentsUnpublished: 0,
+      commentedQuizes:0,
+      commentsUncomented:0
+    };
+
+var errors = [];
+
+
+exports.calculate = function (req, res, next) {
+   models.Quiz.count( req.body.quiz)
+   .then(function (numQuizes) { // número de preguntas
+   statistics.quizes = numQuizes;
+   return models.Comment.count();
+     })
+   .then(function (numComments) { // número de comentarios
+   statistics.comments = numComments;
+  return models.Comment.countUnpublished();
+    })
+  .then(function (numUnpublished) { // número de comentarios sin publicar
+   statistics.commentsUnpublished = numUnpublished;
+return models.Comment.countCommentedQuizes();
+    })
+   .then(function (numCommented) { // número de preguntas con comentario
+     statistics.commentedQuizes = numCommented;
+    return models.CountUnCommentedQuizes();
+    })
+  .catch(function (err) { errors.push(err); })
+  .finally(function (error) {
+  next();
+  });
+};
+
+
+exports.listar = function(req, res) {
+   res.render('quizes/statistics', { statistics: statistics, errors:[] })
+  };
